70352b35645bc9e171ff2c71cfb7a3ed71198e49
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-10-14 23:56:23 +0900

    add Login and Signup containers to router

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import NotFound from '../../components/NotFound';
+import Login from '../Login';
+import Signup from '../Signup';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
+        <Switch style={{ display: 'flex', flex: '1' }}>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route component={NotFound} />
+        </Switch>
       </BrowserRouter>
     );
   }
