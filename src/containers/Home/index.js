5d17a80870a435efba0122dc1431bdfd106e65cd
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-10-14 22:23:12 +0900

    set up fontawesome and bootstrap

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,10 @@
 // @flow
 import React from 'react';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faGhost } from '@fortawesome/free-solid-svg-icons'
+//
+// library.add(faGhost)
 
 const Home = () =>
   <div>Home</div>;
