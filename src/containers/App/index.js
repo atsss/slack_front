632db5c812b69574106592f174a744522cd05238
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-10-16 03:56:03 +0900

    create room and sidbar components

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -10,12 +10,15 @@ import Login from '../Login';
 import Signup from '../Signup';
 import MatchAuthenticated from '../../components/MatchAuthenticated';
 import RedirectAuthenticated from '../../components/RedirectAuthenticated';
+import Sidebar from '../../components/Sidebar';
+import Room from '../Room';
 
 type Props = {
   authenticate: () => void,
   unauthenticate: () => void,
   isAuthenticated: boolean,
   willAuthenticate: boolean,
+  currentUserRooms: Array,
 }
 
 class App extends Component {
@@ -32,15 +35,21 @@ class App extends Component {
   props: Props
 
   render() {
-    const { isAuthenticated, willAuthenticate } = this.props;
+    const { isAuthenticated, willAuthenticate, currentUserRooms } = this.props;
     const authProps = { isAuthenticated, willAuthenticate };
 
     return (
       <Router history={history}>
         <Switch style={{ display: 'flex', flex: '1' }}>
+          {isAuthenticated &&
+            <Sidebar
+              rooms={currentUserRooms}
+            />
+          }
           <MatchAuthenticated exact path="/" component={Home} {...authProps} />
           <RedirectAuthenticated path="/login" component={Login} {...authProps} />
           <RedirectAuthenticated path="/signup" component={Signup} {...authProps} />
+          <MatchAuthenticated pattern="/r/:id" component={Room} {...authProps} />
           <Route component={NotFound} />
         </Switch>
       </Router>
@@ -52,6 +61,7 @@ export default connect(
   state => ({
     isAuthenticated: state.session.isAuthenticated,
     willAuthenticate: state.session.willAuthenticate,
+    currentUserRooms: state.rooms.currentUserRooms,
   }),
   { authenticate, unauthenticate }
 )(App);
