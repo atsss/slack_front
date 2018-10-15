70f5070f25533786ed4dda1258daf98598aae874
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-10-16 03:18:25 +0900

    create room action and reducer

--- src/actions/session.js
+++ src/actions/session.js
@@ -1,10 +1,12 @@
 import { reset } from 'redux-form';
 import api from '../api';
 import history from '../histroy'
+import { fetchUserRooms } from './rooms';
 
 function setCurrentUser(dispatch, response) {
   localStorage.setItem('token', JSON.stringify(response.meta.token));
   dispatch({ type: 'AUTHENTICATION_SUCCESS', response });
+  dispatch(fetchUserRooms(response.data.id));
 }
 
 export function login(data, router) {
